7e762386ea01f867114b2b11bf3913b9c417c9df
OscarWozniak <ansgarykrk93@gmail.com>
2018-07-07 04:08:26 +0200

    skipped files

---
+++ src/main/java/org/github/Assignees.java
@@ -0,0 +1,14 @@
+package org.github;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Assignees {
+  String login;
+  int id;
+}
